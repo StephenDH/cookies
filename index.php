845a918f3feb6eb48f4df6a485bb954dd65d2674
Stephen D'Hondt <stephendhondt@hotmail.be>
2014-05-31 14:54:56 +0200

    Werkende oefening cookies.

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+	$userid = "";
+
+	if(isset($_POST['name'])){
+		setcookie('userid', $_POST['name'], strtotime('+1 day'));
+		$userid = $_POST['name'];
+	}else if(isset($_COOKIE['userid'])) {
+		$userid = $_COOKIE['userid'];
+	}
+
+	if(isset($_POST["kill"])){
+		setcookie("userid", "", strtotime('-1 year'));
+	}
+ ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Oefening PHP </title>
+</head>
+<body>
+	<?php 
+		if($userid != ""){
+			echo "<h1>hallo "."$userid"."</h1>";			
+		}
+	 ?>
+	<form method="post">
+		<input type="text" name="name"/>
+		<input type="submit" value="Submit">
+	</form>
+	<button type="button" value="kill">Kill Cookie</button>
+</body>
+</html> 
